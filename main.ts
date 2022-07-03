e9ce5560907247413a4c0072da8ec11d66fe2dfd
BillJr99 <BillJr99@users.noreply.github.com>
2022-07-03 17:26:58 -0400

    test params

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+ESP8266VarIOT.connectWifi(
+SerialPin.P0,
+SerialPin.P1,
+BaudRate.BaudRate115200,
+"",
+""
+)
 basic.forever(function () {
-	
+    if (ESP8266VarIOT.isWifiConnected()) {
+        ESP8266VarIOT.sendVarIOTTelemetry(
+        "https://rpi4-variot",
+        "443",
+        "",
+        "temperature",
+        30
+        )
+    }
 })
