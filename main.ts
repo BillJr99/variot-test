8056265a768055312598a28087fe3393bf6763e3
BillJr99 <BillJr99@users.noreply.github.com>
2022-07-03 18:23:35 -0400

    New plugin format

--- main.ts
+++ main.ts
@@ -8,11 +8,11 @@ BaudRate.BaudRate115200,
 basic.forever(function () {
     if (ESP8266VarIOT.isWifiConnected()) {
         ESP8266VarIOT.sendVarIOTTelemetry(
-        "https://rpi4-variot",
-        "443",
-        "",
-        "temperature",
-        30
+        "rpi4-variot",
+        "5000",
+        "mongan",
+        "temp",
+        "50"
         )
     }
 })
